1f4fa08a16569399c69cd32d815881ed3e506d6a
CanaanGM <reakak1@gmail.com>
2023-04-19 15:47:32 +0300

    Add: Basket Repo && Controller

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 namespace Basket.API
 {
     public class Program
@@ -12,6 +14,14 @@ namespace Basket.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+
+
 
             var app = builder.Build();
 
