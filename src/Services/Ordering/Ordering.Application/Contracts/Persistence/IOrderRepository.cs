ce359980cafbf7cd6f497adc3650349bb5fa1c9f
CanaanGM <reakak1@gmail.com>
2023-04-21 02:53:08 +0300

    Add: Ordering-App layer

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Domain.Entites;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    internal interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
