ff5856808256c41e0823f634aec515217952d1ec
CanaanGM <reakak1@gmail.com>
2023-04-21 05:43:00 +0300

    Add: APi layer

---
+++ src/Services/Ordering/Ordering.API/Mapping/OrderingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Ordering.API.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            //CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Hosting;
+
+using Ordering.API.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
+
 namespace Ordering.API
 {
     public class Program
@@ -6,12 +13,21 @@ namespace Ordering.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+
+            builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAutoMapper(typeof(Program));
+
+
 
             var app = builder.Build();
 
@@ -26,6 +42,15 @@ namespace Ordering.API
 
 
             app.MapControllers();
+
+            app.MigrateDatabase<OrderContext>( (context, service) =>
+            {
+                var logger = service.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed
+                    .SeedAsync(context, logger)
+                    .Wait();
+            } );
+
 
             app.Run();
         }
