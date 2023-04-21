40b552223844ee9cdd550298ba82adea2f2c0f5d
CanaanGM <reakak1@gmail.com>
2023-04-21 05:44:04 +0300

    fixed typo in namespace

--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ordering.Domain.Entites
+namespace Ordering.Domain.Entities
 {
     public class Order : EntityBase
     {
