52fe9f6abd110f80c94ea59a38183af3c50df368
CanaanGM <reakak1@gmail.com>
2023-04-23 19:40:47 +0300

    Add: Ocelot BFF

--- src/ApiGateways/OceltotApiGateway/Program.cs
+++ src/ApiGateways/OceltotApiGateway/Program.cs
@@ -9,6 +9,18 @@ namespace OceltotApiGateway
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                //if (hostingContext.HostingEnvironment.IsEnvironment("Local"))
+                //    config.AddJsonFile("ocelot.Local.json");
+
+                config.AddJsonFile(
+                    path: $"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", 
+                    optional: true, 
+                    reloadOnChange: true
+                );
+            });
+            
             builder.WebHost.ConfigureLogging((hostingContext, loggingBuilder) => {
                 loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
                 loggingBuilder.AddConsole();
