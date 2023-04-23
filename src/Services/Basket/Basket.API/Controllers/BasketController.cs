28abf2e8ab2572e7a5296af9bb7b2949ef0b68c1
CanaanGM <reakak1@gmail.com>
2023-04-23 14:29:21 +0300

    Clean up

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -49,26 +49,6 @@ namespace Basket.API.Controllers
         {
             var basket = await _repository.GetBasket(userName);
 
-            //await _publishEndpoint.Publish(new BasketCheckoutEvent
-            //{
-            //    UserName = "Dante",
-            //    TotalPrice = 10,
-            //    FirstName = "dante",
-            //    LastName = "sparda",
-            //    EmailAddress = "dante@test.com",
-            //    AddressLine = "redgrave",
-            //    Country = "???",
-            //    State = "???",
-            //    ZipCode = "???",
-            //    CardName = "Demon Red",
-            //    CardNumber = "666 666 777 777",
-            //    Expiration = "67-98",
-            //    CVV = "879",
-            //    PaymentMethod = 2
-
-            //});
-
-
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
@@ -103,24 +83,18 @@ namespace Basket.API.Controllers
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
         {
-            // get existing basket with total price 
-            // Create basketCheckoutEvent -- Set TotalPrice on basketCheckout eventMessage
-            // send checkout event to rabbitmq
-            // remove the basket
 
-            // get existing basket with total price
+
             var basket = await _repository.GetBasket(basketCheckout.UserName);
             if (basket == null)
             {
                 return BadRequest();
             }
 
-            // send checkout event to rabbitmq
             var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
             eventMessage.TotalPrice = basket.TotalPrice;
             await _publishEndpoint.Publish(eventMessage);
 
-            // remove the basket
             await _repository.DeleteBasket(basket.UserName);
 
             return Accepted();
